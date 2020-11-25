e4bc75a6f7c946236717b36d122826f846741dc5
CB17112 Syahril.A <68783574+Syah17@users.noreply.github.com>
2020-11-25 23:16:43 +0800

    Update Twitter_app.py

--- Twitter_app.py
+++ Twitter_app.py
@@ -20,8 +20,8 @@ def keyword():
         
     word_cld=Twitter_Sentiment_Analysis.plotting.show_wordcloud(word['tweets'])
     negative_positive_plot=Twitter_Sentiment_Analysis.plotting.sentiment(word)
-    
-    return render_template('Twitter.html',prediction=word['tweets'].head(),Top_Five_Tweets="Raw Tweets:",plot=word_cld,Word_cloud="Word Cloud for common words used in the Tweets",plot_polarity=polarity,polatiry_plot="Subjectivity against Polarity",plot_sentiment=negative_positive_plot,Sentiment="Count of Positive, Negative and Neutral Tweets")
+    pie_chart=Twitter_Sentiment_Analysis.plotting.pie(word) 
+    return render_template('Twitter.html',prediction=word['tweets'].head(),Top_Five_Tweets="Raw Tweets:",plot=word_cld,Word_cloud="Common words used",plot_polarity=polarity,polatiry_plot="Subjectivity against Polarity",plot_sentiment=negative_positive_plot,Sentiment="Tweets Sentiment")
     
 
 
